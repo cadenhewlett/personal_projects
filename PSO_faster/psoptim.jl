3c92ee6b1921417171a645f72568c4a7a153b1b0
Caden Hewlett <thetroutmanishere@gmail.com>
2025-02-24 10:40:19 -0800

    learning about kwargs

--- PSO_faster/psoptim.jl
+++ PSO_faster/psoptim.jl
@@ -3,7 +3,24 @@
 # --------------------- #
 using Distributions
 
-function psoptim(par, fn, lower = -1, upper = 1)
+function psoptim(par, fn::Function, lower = -1, upper = 1; kwargs... ) 
+    # ------------------- #
+    # -- Default Param -- #
+    # ------------------- #
+    default_controls = Dict(
+        :control1 => 100,
+        :control2 => 200,
+        :control3 => 300
+    )
+    # extract default control variable names
+    con_keys = keys(default_controls)
+    # check if we have any illegal keys
+    illegal_keys = setdiff(keys(kwargs), con_keys)
+    if !isempty(illegal_keys)
+        error("Invalid Control Argument(s): ", join(illegal_keys, ", "))
+    end
+    # otherwise merge
+    controls = merge(default_controls, kwargs)
     # ------------------- #
     # ------ LOCAL ------ #
     # ------------------- #
@@ -12,3 +29,4 @@ function psoptim(par, fn, lower = -1, upper = 1)
     end
     return(2)
 end    
+# println(psoptim(2, mean, control2 = 10))
