97695c62f304d0458828793b0161257ca8130f29
Caden Hewlett <thetroutmanishere@gmail.com>
2025-03-12 13:55:13 -0700

    betakern optimized

---
+++ PSO_faster/betakern_test.jl
@@ -0,0 +1,32 @@
+using LinearAlgebra
+using Pkg
+using Random
+using Distributions
+using Base.Threads
+using Plots
+function mnunif(m::Int, n::Int, lower, upper)
+    # ensure bounds are column vectors
+    lower = isa(lower, Number) ? fill(lower, m) : lower
+    upper = isa(upper, Number) ? fill(upper, m) : upper
+    # generate m*n matrix scaled to the boundaries
+    return rand(m, n) .* (upper .- lower) .+ lower
+end
+p_s = 30
+p_p = 0.35
+ # generate SxS matrix of informant indices
+ L = rand(p_s, p_s) .<= p_p
+ # ensure that each particle is its own informant
+ L[diagind(L)] .= 1
+
+
+ beta_weights = function(N, B; A = 1)
+    K = (x -> pdf(Beta(A, B), x)).(collect(range(0, 1, length=N)))
+    return K/sum(K)
+ end
+
+ betafaster = function(N, B)
+    K = (x -> B*(1-x)^(B-1)).(collect(range(0, 1, length=N)))
+    return K/sum(K)
+ end
+ 
+vcat(beta_weights(5, 3)', betafaster(5,3)')
