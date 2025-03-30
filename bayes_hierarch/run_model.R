116b76c3476f814832b40068dc5cbf6bf3c88ca0
Caden Hewlett <thetroutmanishere@gmail.com>
2025-03-29 22:31:49 -0700

    added weighted average proof and (tried) to introduce Roberts

--- bayes_hierarch/run_model.R
+++ bayes_hierarch/run_model.R
@@ -9,7 +9,6 @@ library(here)
 set.seed(1928)
 # df <- read.csv("C:/Users/thetr/OneDrive/Desktop/Work/TA_STAT_200/pokedex.csv")
 # generate heteroskedastic data
-setwd("C://Users//thetr//OneDrive//Desktop//Misc//Personal Projects//bayes_hierarch")
 rstan_options(auto_write = TRUE)
 x <- rnorm(100, mean = 0, sd = 1)
 y <- 3*x - 4 + rnorm(100, mean = 0, sd = 6 + 1.5*x)
@@ -17,6 +16,7 @@ y <- 3*x - 4 + rnorm(100, mean = 0, sd = 6 + 1.5*x)
 df <- tibble(x = x, y = y)
 n <- length(y)
 # run HMC
+here::here()
 mod_b <- stan(
   "mod_b.stan",
   model_name = "mod_b",
@@ -28,5 +28,5 @@ mod_b <- stan(
   iter = 10000,   
   warmup = 3000
 )
-
+?sampling
 saveRDS(mod_b, "mod_b.rds")
